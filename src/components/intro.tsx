8791be501bbe5333cdce1fefe0daccb8436f9e70
Dan Cargill <dan@cargill.dev>
2025-02-13 13:07:25 +0000

    new job new me (#8)

--- src/components/intro.tsx
+++ src/components/intro.tsx
@@ -1,34 +1,74 @@
+'use client'
+
 import { links } from '@/lib/site'
+import { motion } from 'motion/react'
+
 import Link from 'next/link'
+import { useState } from 'react'
+
+import Sparkles from 'react-sparkle'
 
 export default function Intro() {
-  const renderLinks = () => {
-    return (
-      <ul className='mt-5 flex space-x-4'>
-        {links.map(link => (
-          <li key={link.href}>
-            <Link
-              href={link.href}
-              target='_blank'
-              className='text-foreground/70 hover:text-foreground transition-colors duration-300'
-            >
-              {link.text}
-            </Link>
-          </li>
-        ))}
-      </ul>
-    )
-  }
+  const [showSecondLine, setShowSecondLine] = useState(false)
+
+  const renderLinks = () => (
+    <ul className='mt-5 flex space-x-4'>
+      {links.map(link => (
+        <li key={link.href}>
+          <Link
+            href={link.href}
+            target='_blank'
+            className='text-foreground/70 hover:text-foreground transition-colors duration-300'
+          >
+            {link.text}
+          </Link>
+        </li>
+      ))}
+    </ul>
+  )
+
   return (
     <>
       <h1 className='mt-5 font-serif text-4xl'>
         <b className='text-foreground block'>
           Hey, I&apos;m <em className='text-primary not-italic'>Dan</em>.
         </b>
-        <span className='text-foreground/40'>Playing the internet</span>
+        <span className='relative inline-block text-foreground/40 font-serif'>
+          {showSecondLine ? (
+            <motion.span
+              initial={{ opacity: 0 }}
+              animate={{ opacity: 1 }}
+              transition={{ duration: 0.75 }}
+            >
+              Playing with <span className='text-tertiary'>Apps</span> &{' '}
+              <div className='text-secondary italic relative inline-block'>
+                AI
+                <Sparkles
+                  color='yellow'
+                  flicker
+                  flickerSpeed='slowest'
+                  count={5}
+                  overflowPx={10}
+                />
+              </div>
+            </motion.span>
+          ) : (
+            <>
+              Playing the internet
+              <motion.div
+                className='absolute left-0 top-1/2 h-[2px] w-full bg-secondary origin-left'
+                initial={{ scaleX: 0 }}
+                animate={{ scaleX: 1 }}
+                transition={{ duration: 1.5, delay: 2 }}
+                onAnimationComplete={() => setShowSecondLine(true)}
+              />
+            </>
+          )}
+        </span>
         <br />
         <b className='text-foreground'>in London, UK</b>.
       </h1>
+
       <nav>{renderLinks()}</nav>
     </>
   )
